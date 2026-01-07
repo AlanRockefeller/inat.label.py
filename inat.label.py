0e3567e61ae7a9a769765b9cbbce80842d21b729
AlanRockefeller <alanrockefeller@gmail.com>
2026-01-07 11:50:08 -0800

    fix bugs

--- inat.label.py
+++ inat.label.py
@@ -1238,6 +1238,10 @@ def create_inaturalist_label(observation_data, iconic_taxon_name, show_common_na
     if voucher_numbers:
         label.append(("Voucher Number(s)", voucher_numbers))
 
+    accession_number = get_field_value(observation_data, 'Accession Number')
+    if accession_number:
+        label.append(("Accession Number", accession_number))
+
     mushroom_observer_url = get_field_value(observation_data, 'Mushroom Observer URL')
     # Avoid duplicating the MO URL if this is a Mushroom Observer observation
     if mushroom_observer_url and not (isinstance(obs_number, str) and obs_number.startswith("MO")):
@@ -1258,7 +1262,8 @@ def create_inaturalist_label(observation_data, iconic_taxon_name, show_common_na
                 label.append((field_name, value))
 
     if custom_remove:
-        label = [item for item in label if item[0] not in custom_remove]
+        remove_set = {n.lower() for n in custom_remove}
+        label = [item for item in label if item[0].lower() not in remove_set]
 
     return label, iconic_taxon_name
 
@@ -1276,7 +1281,7 @@ def find_non_ascii_chars(labels):
                         non_ascii_chars.add(char)
     return non_ascii_chars
 
-def create_pdf_content(labels, filename, no_qr=False):
+def create_pdf_content(labels, filename, no_qr=False, title_field=None):
     """Render labels into a two-column PDF at the given filename.
 
     Expects labels as an iterable of (label_fields, iconic_taxon_name). Adds a QR code when a URL is present.
@@ -1318,6 +1323,14 @@ def create_pdf_content(labels, filename, no_qr=False):
         fontSize=12,
         leading=14
     )
+    title_normal_style = ParagraphStyle(
+        'TitleNormal',
+        parent=styles['Title'],
+        fontName=base_font,
+        fontSize=16,
+        leading=18,
+        alignment=1  # Centered
+    )   
     story = []
 
     for label, iconic_taxon_name in labels:
@@ -1327,12 +1340,24 @@ def create_pdf_content(labels, filename, no_qr=False):
             (value for field, value in label
              if field in ("iNaturalist URL", "Mushroom Observer URL")),
             None)
+        
+        if title_field:
+            for field, value in label:
+                if field == title_field:
+                    title_value = value.replace('__BOLD_START__', '<b>').replace('__BOLD_END__', '</b>')
+                    title_value = title_value.replace('__ITALIC_START__', '<i>').replace('__ITALIC_END__', '</i>')
+                    p = Paragraph(f"<b>{title_value}</b>", title_normal_style)
+                    pre_notes_content.append(p)
+                    pre_notes_content.append(Spacer(1, 0.1*inch))
+                    break
 
         for field, value in label:
             if field == "Notes":
                 notes_value = value
                 continue
-            if field == "Scientific Name":
+            if title_field and field == title_field:
+                continue
+            elif field == "Scientific Name":
                 sci_html = value.replace('__ITALIC_START__','<i>').replace('__ITALIC_END__','</i>')
                 p = Paragraph(f"<b>{field}:</b> {sci_html}", custom_normal_style)
                 pre_notes_content.append(p)
@@ -1787,29 +1812,25 @@ def main():
     parser.add_argument("--no-qr", action="store_true", help="Omit QR code from PDF and RTF labels")
     parser.add_argument("--minilabel", action="store_true", help="Generate minilabels with only observation number and QR code")
     parser.add_argument("--omit-notes", action="store_true", help="Omit the Notes field from all labels")
+    parser.add_argument("--title", type=str, default=None, help="Field to use as title (only for PDF output)")
     parser.add_argument(
         '--custom',
-        nargs='+',
-        help='Add or remove fields from the default label format. Use "+" to add and "-" to remove. For example: --custom "+My Field" -Observer',
+        help='Add or remove fields from the default label format. Use "+" to add and "-" to remove. For example: --custom "+My Field, -Observer"',
     )
+    
 
     args = parser.parse_args()
 
     fields_to_add = []
     fields_to_remove = []
     if args.custom:
-        i = 0
-        while i < len(args.custom):
-            arg = args.custom[i]
+        custom_items = [item.strip() for item in args.custom.split(',')]
+        for arg in custom_items:
+            if not arg:
+                continue
             if arg.startswith('+') or arg.startswith('-'):
                 mod = arg[0]
-                field_parts = [arg[1:]]
-                i += 1
-                while i < len(args.custom) and not args.custom[i].startswith('+') and not args.custom[i].startswith('-'):
-                    field_parts.append(args.custom[i])
-                    i += 1
-                
-                field_name = ' '.join(part for part in field_parts if part)
+                field_name = arg[1:].strip()
                 if not field_name:
                     continue
 
@@ -1818,13 +1839,18 @@ def main():
                 elif mod == '-':
                     fields_to_remove.append(field_name)
             else:
-                print(f"Error: Invalid format for --custom. Field options must start with '+' or '-'. Found: '{arg}'")
-                i += 1
+                print_error(f"Error: Invalid format for --custom. Field options must start with '+' or '-'. Found: '{arg}'")
+                sys.exit(2)
 
 
     # If no arguments are provided, show help and exit
     if len(sys.argv) == 1:
         parser.print_help()
+        sys.exit(1)
+
+    # User can not use 'Notes" as title field
+    if args.title == "Notes":
+        parser.error("argument --title: 'Notes' field can not be used as title")
         sys.exit(1)
 
     # Define rtf_mode and pdf_mode based on whether --rtf or --pdf argument is provided
@@ -1974,7 +2000,7 @@ def main():
                 else:
                     print(f"RTF file created: {basename}", flush=True)
             elif pdf_mode:
-                create_pdf_content(labels, args.pdf, no_qr=args.no_qr)
+                create_pdf_content(labels, args.pdf, no_qr=args.no_qr, title_field=args.title)
                 try:
                     size_bytes = os.path.getsize(args.pdf)
                     size_kb = (size_bytes + 1023) // 1024
